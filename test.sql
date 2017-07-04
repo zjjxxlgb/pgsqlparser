ab0b0f269a56d8b400d480dce396157e86efd3cb
zjjxxlgb <zjjxxl@163.com>
2017-07-04 11:08:11 +0800

    test file
    test file

---
+++ test.sql
@@ -0,0 +1,18 @@
+alter table  altertbdpc  DROP c1,add c2 varchar(54);
+
+drop table droptbl;
+
+delete from deletetb;
+
+update uptb set fg=3;
+
+truncate table truncatetb;
+
+select * from selecttb;
+CREATE  INDEX  ON tableindex(cin);
+alter table  testndpc add c2 varchar(54);
+DROP INDEX dpindex;
+CREATE TABLE t_table2 (
+    city            varchar(80),
+    temp_lo         int        
+);
---
+++ test2.sql
@@ -0,0 +1,10 @@
+drop table droptbl;
+
+alter table  testndpc add c2 varchar(54);
+
+CREATE TABLE t_table2 (
+    city            varchar(80),
+    temp_lo         int        
+);
+
+delete fr2om deletetb;
